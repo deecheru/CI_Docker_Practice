498e0e12e2fcf74d4ce5029830a2e4a4b4a87816
Deepika <chedeepika1997@gmail.com>
2025-10-22 17:13:41 -0500

    updated tests commit

--- app_services/tests.py
+++ app_services/tests.py
@@ -3,7 +3,7 @@ from rest_framework.test import  APITestCase
 from rest_framework import  status
 class HelloAPiTestcase(APITestCase):
     def test_hello_api(self):
-        response = self.client.request('api/hello/')
+        response = self.client.get('api/hello/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data,{"maessage: Hello, World"})
 
