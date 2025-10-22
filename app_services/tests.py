b16b0a53bac5f7dfbbbb97b0fb3bb7e80a6ab4a0
Deepika <chedeepika1997@gmail.com>
2025-10-22 17:09:50 -0500

    updated tests commit

--- app_services/tests.py
+++ app_services/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from rest_framework.test import  APITestCase
+from rest_framework import  status
+class HelloAPiTestcase(APITestCase):
+    def test_hello_api(self):
+        response = self.client.request('api/hello/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,{"maessage: Hello, World"})
+
 
 # Create your tests here.
